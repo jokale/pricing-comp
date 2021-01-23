d0da0db4887663d8dfb179428ee3704aaee9ef3e
jokale <kalemajoanna@gmail.com>
2021-01-23 19:46:24 +0000

    add some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import bottom from './bottom.svg'
+import top from './top.svg'
 
-function App() {
-  return (
+class App extends React.Component {
+  render(){
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Pricing</h1>
+        <div id="card1">
+          <h2>Basic</h2>
+
+        </div>
+         <div id="card2">
+          <h2>Professional</h2>
+        </div>
+         <div id="card3">
+           <h2>Master</h2>
+
+          </div>
+         <img src={top} className="App-top" alt="top" />
+         <img src={bottom} className="App-bottom" alt="bottom" />
+
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
