ef92190d922a482c97d08c06f11267d1aa41b9f9
jokale <kalemajoanna@gmail.com>
2021-02-01 11:56:07 +0000

    add styling to cards

--- src/App.js
+++ src/App.js
@@ -15,29 +15,41 @@ class App extends React.Component {
 </label>
      <h5>Anually</h5>
         <div id="card1">
-          <h2>Basic</h2>
-          <h1>$19.99</h1>
-          <h6>500 GB Storage</h6> <br></br>
-           <h6>2 Users Allowed</h6> <br></br>
-           <h6>Send up to 3GB </h6> <br></br>
+          <h2 id="heading2">Basic</h2>
+          <h1 id="price">$199.99</h1>
+          <hr class="solid"/>
+          <h6>500 GB Storage</h6>
+          <hr class="solid"/>
+           <h6>2 Users Allowed</h6>
+           <hr class="solid"/>
+           <h6>Send up to 3GB </h6> 
+           <hr class="solid"/>
            <button id="purple">Learn More</button>
 
         </div>
          <div id="card2">
-          <h2>Professional</h2>
-          <h1>$24.99</h1>
-          <h6>1 TB Storage</h6> <br></br>
-           <h6>5 Users Allowed</h6> <br></br>
-           <h6>Send up to 10GB </h6> <br></br>
+          <h2 id="heading2" >Professional</h2>
+          <h1 id="price">$249.99</h1>
+          <hr class="solid"/>
+          <h6>1 TB Storage</h6> 
+          <hr class="solid"/>
+           <h6>5 Users Allowed</h6> 
+           <hr class="solid"/>
+           <h6>Send up to 10GB </h6> 
+           <hr class="solid"/>
            <button id="white">Learn More</button>
 
         </div>
          <div id="card3">
-           <h2>Master</h2>
-           <h1>$39.99</h1>
-           <h6>2 TB Storage</h6> <br></br>
-           <h6>10 Users Allowed</h6> <br></br>
-           <h6>Send up to 29GB </h6> <br></br>
+           <h2 id="heading2">Master</h2>
+           <h1 id="price">$399.99</h1>
+           <hr class="solid"/>
+           <h6>2 TB Storage</h6> 
+           <hr class="solid"/>
+           <h6>10 Users Allowed</h6> 
+           <hr class="solid"/>
+           <h6>Send up to 29GB </h6> 
+           <hr class="solid"/>
            <button id="purple">Learn More</button>
           </div>
          <img id="top"src={top} className="App-top" alt="top" />
