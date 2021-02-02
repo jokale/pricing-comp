4d03ba1bda02c528e72456ff1ad595c367b34022
jokale <kalemajoanna@gmail.com>
2021-02-02 17:14:37 +0000

    add styling/positionings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render(){
       return (
     <div className="App">
-          <h1>Our Pricing</h1>
+          <h1 id="main">Our Pricing</h1>
           <h5>Monthly</h5>
           <label class="switch">
   <input type="checkbox"/> 
@@ -15,40 +15,40 @@ class App extends React.Component {
 </label>
      <h5>Anually</h5>
         <div id="card1">
-          <h2 id="heading2">Basic</h2>
+          <h2 id="heading1">Basic</h2>
           <h1 id="price">$199.99</h1>
-          <hr class="solid"/>
-          <h6>500 GB Storage</h6>
-          <hr class="solid"/>
-           <h6>2 Users Allowed</h6>
+          <hr class="solid1"/>
+          <h5 id="h51">500 GB Storage</h5>
+          <hr class="solid2"/>
+           <h5 id="h52">2 Users Allowed</h5>
            <hr class="solid"/>
-           <h6>Send up to 3GB </h6> 
-           <hr class="solid"/>
+           <h5 id="h53">Send up to 3GB </h5> 
+           <hr class="solid"/><br></br>
            <button id="purple">Learn More</button>
 
         </div>
          <div id="card2">
           <h2 id="heading2" >Professional</h2>
-          <h1 id="price">$249.99</h1>
-          <hr class="solid"/>
-          <h6>1 TB Storage</h6> 
-          <hr class="solid"/>
-           <h6>5 Users Allowed</h6> 
+          <h1 id="price2">$249.99</h1>
+          <hr class="solid1w"/>
+          <h5 id="h51w">1 TB Storage</h5> 
+          <hr class="solid2"/>
+           <h5 id="h52w">5 Users Allowed</h5> 
            <hr class="solid"/>
-           <h6>Send up to 10GB </h6> 
+           <h5 id="h53w">Send up to 10GB </h5> 
            <hr class="solid"/>
            <button id="white">Learn More</button>
 
         </div>
          <div id="card3">
-           <h2 id="heading2">Master</h2>
-           <h1 id="price">$399.99</h1>
+           <h2 id="heading3">Master</h2>
+           <h1 id="price3">$399.99</h1>
+           <hr class="solid1"/>
+           <h5 id="h51">2 TB Storage</h5> 
+           <hr class="solid2"/>
+           <h5 id="h52">10 Users Allowed</h5> 
            <hr class="solid"/>
-           <h6>2 TB Storage</h6> 
-           <hr class="solid"/>
-           <h6>10 Users Allowed</h6> 
-           <hr class="solid"/>
-           <h6>Send up to 29GB </h6> 
+           <h5 id="h53">Send up to 29GB </h5> 
            <hr class="solid"/>
            <button id="purple">Learn More</button>
           </div>
